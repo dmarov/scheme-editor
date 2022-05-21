f5e18d48ffdd455cee4d999c50330ed7e8d199a2
Dmitry Marov <d.marov94@gmail.com>
2022-05-21 16:52:56 +0300

    mesh wip

--- src/lib/classes/mesh.ts
+++ src/lib/classes/mesh.ts
@@ -14,50 +14,69 @@ export class Mesh implements Drawable {
 
     draw(ctx: DrawingContext): void {
         ctx.width(1);
-        this.drawVerticalLines(ctx);
-        this.drawHorizontalLines(ctx);
+        const horCnt = Math.floor(this.viewportDimensions.x / this.meshGap / 2) + 1;
+        const vertCnt = Math.floor(this.viewportDimensions.y / this.meshGap / 2) + 1;
+
+        this.drawExtraLines(ctx, horCnt, vertCnt);
+        this.drawSecondaryLines(ctx, horCnt, vertCnt);
+        this.drawPrimaryLines(ctx);
     }
 
-    private drawVerticalLines(ctx: DrawingContext) {
-        const horCnt = Math.floor(this.viewportDimensions.x / this.meshGap / 2) + 1;
+    private drawExtraLines(ctx: DrawingContext, horCnt: number, vertCnt: number) {
+        this.drawHorizontalLines(ctx, this.extraColor, horCnt, 1);
+        this.drawVerticalLines(ctx, this.extraColor, vertCnt, 1);
+    }
+
+    private drawSecondaryLines(ctx: DrawingContext, horCnt: number, vertCnt: number) {
+        this.drawHorizontalLines(ctx, this.secondaryColor, horCnt, 5);
+        this.drawVerticalLines(ctx, this.secondaryColor, vertCnt, 5);
+    }
+
+    private drawPrimaryLines(ctx: DrawingContext) {
+        ctx.color(this.primaryColor)
+            .line({
+                x: this.viewportDimensions.x / 2,
+                y: 0
+            }, {
+                x: this.viewportDimensions.x / 2,
+                y: this.viewportDimensions.y,
+            }).line({
+                x: 0,
+                y: this.viewportDimensions.y / 2,
+            }, {
+                x: this.viewportDimensions.x,
+                y: this.viewportDimensions.y / 2,
+            });
+    }
+
+    private drawVerticalLines(ctx: DrawingContext, color: string, vertCnt: number, density: number) {
         const centerX = this.viewportCenter.x;
-        for (let i = -horCnt; i < horCnt; i++) {
-            ctx.color(this.getColorForIndex(i))
+        const length = vertCnt / density;
+        for (let i = -length; i < length; i++) {
+            ctx.color(color)
                 .line({
-                    x: centerX + i * this.meshGap,
+                    x: centerX + i * this.meshGap * density,
                     y: 0
                 }, {
-                    x: centerX + i * this.meshGap,
+                    x: centerX + i * this.meshGap * density,
                     y: this.viewportDimensions.y
                 });
         }
     }
 
-    private drawHorizontalLines(ctx: DrawingContext) {
-        const vertCnt = Math.floor(this.viewportDimensions.y / this.meshGap / 2) + 1;
+    private drawHorizontalLines(ctx: DrawingContext, color: string, horCnt: number, density: number) {
         const centerY = this.viewportCenter.y;
+        const length = horCnt / density;
 
-        for (let i = -vertCnt; i < vertCnt; i++) {
-            ctx.color(this.getColorForIndex(i))
+        for (let i = -length; i < length; i++) {
+            ctx.color(color)
                 .line({
-                    x: 0, y: centerY + i * this.meshGap
+                    x: 0,
+                    y: centerY + i * this.meshGap * density
                 }, {
                     x: this.viewportDimensions.x,
-                    y: centerY + i * this.meshGap
+                    y: centerY + i * this.meshGap * density
                 });
         }
-    }
-
-    private getColorForIndex(i: number) {
-
-        if (i === 0) {
-            return this.primaryColor;
-        }
-
-        if ((i % 5) === 0) {
-            return this.secondaryColor;
-        }
-
-        return this.extraColor;
     }
 }
