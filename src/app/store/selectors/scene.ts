d71a5b07ec3e8bd093133b7148bffba49876466f
Dmitry Marov <d.marov94@gmail.com>
2022-05-21 22:46:10 +0300

    scale wip

--- src/app/store/selectors/scene.ts
+++ src/app/store/selectors/scene.ts
@@ -23,11 +23,16 @@ export const selectRenderingModel = createSelector(
 
         const sq1 = new Square(new Point(50, 50), 100, state.extraColor, state.secondaryColor);
         const sq2 = new Square(new Point(150, -150), 100, state.extraColor, state.secondaryColor);
-        const sq = new Collection([sq1, sq2]);
-        const shl = new ShiftedLayer(state.meshOrigin, sq);
+        const sqs = new Collection([sq1, sq2]);
+
+        const shl = new ShiftedLayer(state.meshOrigin, sqs);
+        const scl = new ScaledLayer({
+            x: state.meshOrigin.x - state.viewportDimensions.x / 2,
+            y: state.meshOrigin.y - state.viewportDimensions.y / 2,
+        }, state.scaleFactor, shl);
 
         const collection = new Collection([mesh, shl]);
-        // const scl = new ScaledLayer(state.scaleFactor, shl);
+
         return collection;
     }
 );
--- src/lib/classes/scaled-layer.ts
+++ src/lib/classes/scaled-layer.ts
@@ -4,15 +4,16 @@ import { Point } from "@/lib/models";
 export class ScaledLayer implements Drawable {
 
     constructor(
+        private readonly origin: Point,
         private readonly scaleFactor: number,
         private readonly entry: Drawable,
     ) { }
 
     draw(ctx: DrawingContext): void {
-        // const prevScale = ctx.getScale();
+        ctx.origin(this.origin);
         ctx.scale(this.scaleFactor);
         this.entry.draw(ctx);
-        ctx.scale(1);
-        // ctx.scale(prevScale);
+        ctx.scale(1 / this.scaleFactor);
+        ctx.origin({x: -this.origin.x, y: -this.origin.y});
     }
 }
