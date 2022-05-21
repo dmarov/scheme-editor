f13daafc6ba1d59843827b1f02b89f61591c0776
Dmitry Marov <d.marov94@gmail.com>
2022-05-21 19:02:43 +0300

    refactored

--- src/lib/classes/mesh.ts
+++ src/lib/classes/mesh.ts
@@ -17,7 +17,7 @@ export class Mesh implements Drawable {
         const horCnt = Math.ceil(this.viewportDimensions.y / this.meshGap) + 1;
         const vertCnt = Math.ceil(this.viewportDimensions.x / this.meshGap) + 1;
 
-        // this.drawExtraLines(ctx, horCnt, vertCnt);
+        this.drawExtraLines(ctx, horCnt, vertCnt);
         // this.drawSecondaryLines(ctx, horCnt, vertCnt);
         this.drawPrimaryLines(ctx);
     }
@@ -51,39 +51,33 @@ export class Mesh implements Drawable {
     }
 
     private drawVerticalLines(ctx: DrawingContext, color: string, vertCnt: number, density: number) {
-        const centerX = this.meshOrigin.x;
-        const length = Math.ceil(vertCnt / density);
-        for (let i = 0; i < length; i++) {
+        const count = Math.ceil(vertCnt / density);
+        const offsetX = Math.ceil(this.meshOrigin.x / this.meshGap);
+
+        for (let i = 0; i < count ; i++) {
             ctx.color(color)
                 .line({
-                    x: this.meshOrigin.x + i * this.meshGap * density,
+                    x: this.meshOrigin.x + (i - offsetX) * this.meshGap * density,
                     y: 0
                 }, {
-                    x: this.meshOrigin.x + i * this.meshGap * density,
+                    x: this.meshOrigin.x + (i - offsetX) * this.meshGap * density,
                     y: this.viewportDimensions.y,
                 });
-                // .line({
-                //     x: this.meshOrigin.x + i * this.meshGap * density,
-                //     y: this.meshOrigin.y - this.viewportDimensions.y / 2
-                // }, {
-                //     x: this.meshOrigin.x + i * this.meshGap * density,
-                //     y: this.meshOrigin.y + this.viewportDimensions.y / 2
-                // });
         }
     }
 
     private drawHorizontalLines(ctx: DrawingContext, color: string, horCnt: number, density: number) {
-        const centerY = this.meshOrigin.y;
         const length = Math.ceil(horCnt / density);
+        const offsetY = Math.ceil(this.meshOrigin.y / this.meshGap);
 
         for (let i = 0; i < length; i++) {
             ctx.color(color)
                 .line({
-                    x: this.meshOrigin.x + this.viewportDimensions.x / 2,
-                    y: this.meshOrigin.y + centerY + i * this.meshGap * density
+                    x: 0,
+                    y: this.meshOrigin.y + (i - offsetY) * this.meshGap * density,
                 }, {
-                    x: this.meshOrigin.x + this.viewportDimensions.x / 2,
-                    y: this.meshOrigin.y + centerY + i * this.meshGap * density
+                    x: this.viewportDimensions.x,
+                    y: this.meshOrigin.y + (i - offsetY) * this.meshGap * density,
                 });
         }
     }
